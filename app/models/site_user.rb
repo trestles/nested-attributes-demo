526ccbe89ac9030946b1cc850bbda3a80ab1fae4
Jonathan Twaddell <jonathan.twaddell@inmobi.com>
2018-08-29 17:25:10 -0700

    ch

---
+++ app/models/site_user.rb
@@ -0,0 +1,2 @@
+class SiteUser < ActiveRecord::Base
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ActiveRecord::Base
   has_many :interests, :through => :interest_users
   has_many :tasks
 
+  has_many :site_users
+  has_many :sites, through: :site_users
+
+
   accepts_nested_attributes_for :address
   accepts_nested_attributes_for :interest_users, :allow_destroy => true
   accepts_nested_attributes_for :tasks, :allow_destroy => true, :reject_if => :all_blank
